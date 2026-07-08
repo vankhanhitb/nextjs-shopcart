1f2a961c89b6d14373bca1b62221c79fe590d02f
Joseph <vankhanhkt12b@gmail.com>
2026-07-08 18:43:25 +0700

    install component from ui-shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div className="bg-shop-light-pink">
       <h1>home</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique quod alias, inventore error nulla et accusantium amet molestias sequi fuga molestiae architecto vero aliquam quia nisi quae consectetur ab voluptatibus.</p>
+      <Button variant="default">Checkout</Button>
     </div>
   );
 }
