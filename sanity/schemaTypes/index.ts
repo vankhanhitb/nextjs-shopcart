fcb92529b74d9ec44fc0a3f098915646acf2a2c2
Joseph <vankhanhkt12b@gmail.com>
2026-07-10 09:51:34 +0700

    Config file database to sanity.io

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,6 +1,14 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import { categoryType } from './categoryType';
+import { addressType } from './addressType';
+import { authorType } from './authorType';
+import { blockContentType } from './blockContentType';
+import { blogCategoryType } from './blogCategoryType';
+import { blogType } from './blogType';
+import { brandType } from './brandType';
+import { orderType } from './orderType';
+import { productType } from './productType';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [categoryType],
+  types: [categoryType, addressType, authorType, blockContentType, blogCategoryType, blogType, brandType, orderType, productType ],
 }
---
+++ sanity/schemaTypes/productType.ts
@@ -0,0 +1,109 @@
+import { TrolleyIcon } from "@sanity/icons"
+import { defineField, defineType } from "sanity"
+
+export const productType = defineType({
+  name: "product",
+  title: "Product",
+  type: "document",
+  icon: TrolleyIcon,
+  fields: [
+    defineField({
+      name: "name",
+      title: "Product Name",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "images",
+      title: "Product Images",
+      type: "array",
+      of: [{type: "image", options: {hotspot: true}}],
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "string",
+    }),
+    defineField({
+      name: "price",
+      title: "Price",
+      type: "number",
+      validation: (Rule) => Rule.required().min(0),
+    }),
+    defineField({
+      name: "discount",
+      title: "Discount",
+      type: "number",
+      validation: (Rule) => Rule.required().min(0),
+    }),
+    defineField({
+      name: "categories",
+      title: "Categories",
+      type: "array",
+      of: [{type: "reference", to: {type: "actegory"}}],
+    }),
+    defineField({
+      name: "stock",
+      title: "Stock",
+      type: "number",
+      validation: (Rule) => Rule.min(0),
+    }),
+    defineField({
+      name: "brand",
+      title: "Brand",
+      type: "reference",
+      to: { type: "brand" },
+    }),
+    defineField({
+      name: "status",
+      title: "Product Status",
+      type: "string",
+      options: {
+        list: [
+          {title: "New", value: "new"},
+          {title: "Hot", value: "hot"},
+          {title: "Sale", value: "sale"},
+        ]
+      }
+    }),
+    defineField({
+      name: "variant",
+      title: "Product Type",
+      type: "string",
+      options: {
+        list: [
+          {title: "Gadget", value: "gadget"},
+          {title: "Appliances", value: "appiances"},
+          {title: "Refrigerators", value: "refigerators"},
+          {title: "Others", value: "others"}
+        ]
+      }
+    }),
+  ],
+  preview: {
+    select: {
+      title: "name",
+      media: "images",
+      subtitle: "price",
+    },
+    prepare(selection){
+      const { title, subtitle, media } = selection;
+      const image = media && media[0];
+      return {
+        title: title,
+        subtitle: `$${subtitle}`,
+        media: image
+      }
+    },
+  }
+})
